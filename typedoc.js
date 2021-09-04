fadb2866520f993700336bbb5c4c7ecd33c81af1
Mikkel Laursen <mlaursen03@gmail.com>
2021-09-03 22:25:22 -0600

    Try using sourcefile-url-prefix plugin

--- typedoc.js
+++ typedoc.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 const { execSync } = require('child_process');
+const { repository } = require('./package.json');
 
 const commitSha = process.env.VERCEL_GIT_COMMIT_SHA;
 let command = 'typedoc';
 if (commitSha) {
-  command = `${command} --gitRevision "${commitSha}"`;
+  command = `${command} --sourcefile-url-prefix "${repository}/blob/${commitSha}/"`;
 }
 
-console.log('git remote -v');
-console.log(execSync('git remote -v', { stdio: 'inherit' }));
 console.log(command);
 execSync(command, { stdio: 'inherit' });

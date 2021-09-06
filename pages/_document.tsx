875d7a97f62928d577e66c531c1ce6d6b5595d6e
Mikkel Laursen <mlaursen03@gmail.com>
2021-09-06 16:29:15 -0600

    chore(website): Maybe added analytics to prod website?

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,16 @@
 import { ReactElement } from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const PRISM_MANUAL_MODE =
-  "window.Prism=window.Prism||{};window.Prism.manual=true";
+const PRISM_HTML = "window.Prism=window.Prism||{};window.Prism.manual=true";
+
+const GA_CODE = process.env.NEXT_PUBLIC_GA_CODE;
+const GA_HREF = `https://www.googletagmanager.com/gtag/js?id=${GA_CODE}`;
+const GA_HTML = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${GA_CODE}');
+`;
 
 export default class MyDocument extends Document {
   render(): ReactElement {
@@ -20,7 +28,13 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <script dangerouslySetInnerHTML={{ __html: PRISM_MANUAL_MODE }} />
+          <script dangerouslySetInnerHTML={{ __html: PRISM_HTML }} />
+          {process.env.NODE_ENV === "production" && GA_CODE && (
+            <>
+              <script async src={GA_HREF} />
+              <script dangerouslySetInnerHTML={{ __html: GA_HTML }} />
+            </>
+          )}
           <NextScript />
         </body>
       </Html>

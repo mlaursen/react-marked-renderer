656f11ff9afacf80ae67bf76849cb0d0965fcba6
Mikkel Laursen <mlaursen03@gmail.com>
2021-09-13 16:56:08 -0600

    trying to fix typedoc

--- scripts/typedoc.ts
+++ scripts/typedoc.ts
@@ -1,11 +1,25 @@
 import { execSync } from "child_process";
 import { repository } from "../package.json";
 
+const loggedExecSync = (command: string): void => {
+  console.log(command);
+  execSync(command, { stdio: "inherit" });
+};
+
 const commitSha = process.env.VERCEL_GIT_COMMIT_SHA;
-let command = "typedoc";
-if (commitSha) {
-  command = `${command} --sourcefile-url-prefix "${repository}/blob/${commitSha}/"`;
+const isNoRemote = execSync("git remote -v").toString().trim().length === 0;
+
+if (commitSha && isNoRemote) {
+  console.log(
+    "A git remote could not be found. Manually setting it to the repositor field in the `package.json`."
+  );
+
+  loggedExecSync(`git remote add origin ${repository}`);
 }
 
-console.log(command);
-execSync(command, { stdio: "inherit" });
+loggedExecSync("typedoc");
+
+if (commitSha && isNoRemote) {
+  loggedExecSync("git remote remove origin");
+  console.log("Removed the manually set origin.");
+}

e9f83f2ccc1ff73853f990c0b12c0b2eb473d828
Mikkel Laursen <mlaursen03@gmail.com>
2021-10-08 19:17:06 -0600

    chore(dev-deps): bump all dev dependencies to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['@mlaursen/eslint-config', 'plugin:@next/next/recommended'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    // disabled due to weirdness of marked library
+    'import/no-named-as-default-member': 0,
   },
   overrides: [
     {
